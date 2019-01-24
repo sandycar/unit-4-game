d0989f2505045bbc405cd6ddd6ff87890ad13fe5
Sandy <sandycnavarro@gmail.com>
2019-01-23 20:41:05 -0800

    Added wins and losses code, reset values to generated number and values of each crystal

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -35,67 +35,126 @@ numbersGenerated()
 $('#randomNumber').html(generatedNum)
 $('#blue').attr("value", crystalsArr[0]);
 $('#blue').on('click', function(){
-	var blueValue = $('#blue').attr("value");
-	console.log("button clicked");
-	currentGemValue+=parseInt(blueValue);
-	$('#gemsCollected').html(currentGemValue);
+	if (currentGemValue<generatedNum){
+		var blueValue = $('#blue').attr("value");
+		console.log("button clicked");
+		currentGemValue+=parseInt(blueValue);
+		$('#gemsCollected').html(currentGemValue);
+	}
+	else if(currentGemValue==generatedNum){
+		numbersGenerated()
+		$('#blue').attr("value", crystalsArr[0]);
+		wins++
+		currentGemValue = 0;
+		
+
+
+	}
+	else if (currentGemValue>generatedNum){
+		numbersGenerated()
+		$('#blue').attr("value", crystalsArr[0]);
+		losses++
+		currentGemValue = 0;
+	}
+display()
 })
 
 // Yellow gem value
 
 $('#yellow').attr("value", crystalsArr[1]);
 $('#yellow').on('click', function(){
-	var yellowValue = $('#yellow').attr("value");
-	console.log("button clicked");
-	currentGemValue+=parseInt(yellowValue);
-	$('#gemsCollected').html(currentGemValue);
+	
+	if (currentGemValue<generatedNum){
+		var yellowValue = $('#yellow').attr("value");
+		console.log("button clicked");
+		currentGemValue+=parseInt(yellowValue);
+		$('#gemsCollected').html(currentGemValue);
+	}
+	else if(currentGemValue==generatedNum){
+		numbersGenerated()
+		$('#yellow').attr("value", crystalsArr[1]);
+		wins++
+		currentGemValue = 0;
+		
+
+
+	}
+	else if (currentGemValue>generatedNum){
+		numbersGenerated()
+		$('#yellow').attr("value", crystalsArr[1]);
+		losses++
+		currentGemValue = 0;
+	}
+display()
 })
+
 
 //Red gem value 
 
 $('#red').attr("value", crystalsArr[2]);
 $('#red').on('click', function(){
+	if(currentGemValue<generatedNum){
 	var redValue = $('#red').attr("value");
 	console.log("button clicked");
 	currentGemValue+=parseInt(redValue);
 	$('#gemsCollected').html(currentGemValue);
+	}
+	else if(currentGemValue==generatedNum){
+		numbersGenerated()
+		$('#red').attr("value", crystalsArr[2]);
+		wins++
+		currentGemValue = 0;
+		
+	}
+	else if (currentGemValue>generatedNum){
+		numbersGenerated()
+		$('#red').attr("value", crystalsArr[2]);
+		losses++
+		currentGemValue = 0;
+	}
+display()
+	
 })
 
 // Green gem value
 $('#green').attr("value", crystalsArr[3]);
 $('#green').on('click', function(){
+
+	if (currentGemValue<generatedNum){
 	var greenValue = $('#green').attr("value");
 	console.log("button clicked");
 	currentGemValue+=parseInt(greenValue);
-	$('#gemsCollected').html(currentGemValue);
-})
-
-// Wins and Losses Tracker
-
-function results(){
-	if (currentGemValue==generatedNum){
-	win++
-	numbersGenerated()
-
-}
-	
-
-if (currentGemValue>generatedNum){
-	losses++
-	numbersGenerated()
-
+	$('#gemsCollected').html(currentGemValue);	
+	}
+	else if(currentGemValue==generatedNum){
+		numbersGenerated()
+		$('#green').attr("value", crystalsArr[3]);
+		wins++
+		currentGemValue = 0;
+		
+	}
+	else if (currentGemValue>generatedNum){
+		numbersGenerated()
+		$('#green').attr("value", crystalsArr[3]);
+		losses++
+		currentGemValue = 0;
+	}
 display()
-}
-
-}
-
+	
+})
 
 
 
 function display(){
 	document.getElementById('wins').innerHTML = wins;
  	document.getElementById('losses').innerHTML = losses;
+ 	document.getElementById('randomNumber').innerHTML = generatedNum;
+ 	document.getElementById('gemsCollected').innerHTML = currentGemValue;
+
 }
+
+
+
 	
 
 
